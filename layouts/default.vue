49ca8c2871e6734e2f7a623fbbee6f2a6032e099
varsharoberoi <42732605+varsharoberoi@users.noreply.github.com>
2022-10-18 15:38:26 +0530

    Update default.vue
    Scroll effect given

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,22 @@
 <script>
 import headerComponent from "/@/components/header.vue";
 import footerComponent from "/@/components/footer.vue";
+import { IonPage, IonContent } from "@ionic/vue";
 export default {
   components: {
     headerComponent: headerComponent,
-    footerComponent: footerComponent
-  }
+    footerComponent: footerComponent,
+    IonPage,
+    IonContent,
+  },
 };
 </script>
 <template>
-  <headerComponent></headerComponent>
-    <tez-slot></tez-slot>
-  <footerComponent></footerComponent>
+  <ion-page>
+    <ion-content :scroll-y="true">
+      <headerComponent></headerComponent>
+      <tez-slot></tez-slot>
+      <footerComponent></footerComponent>
+    </ion-content>
+  </ion-page>
 </template>
